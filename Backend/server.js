3fce0a247386d987d780a3391c1ef3c83226dec2
Manjunath063 <manjunathjangam198@gmail.com>
2025-06-23 09:04:29 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -119,5 +119,5 @@ app.post('/api/jobs', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.60.244.180:${port}`);
+    console.log(`Server running at http://51.20.181.219:${port}`);
 });
